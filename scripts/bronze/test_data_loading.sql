a8ae89e654ec2e08fcec80926fd1e22d94b5e3af
Kavinda S. Rajapaksha <125864556+KavindaRajapaksha@users.noreply.github.com>
2025-09-06 22:40:19 +0530

    Add execution of load_bronze procedure in SQL script

--- scripts/bronze/test_data_loading.sql
+++ scripts/bronze/test_data_loading.sql
@@ -5,3 +5,5 @@ SELECT COUNT(*) AS crm_sales_details_rows FROM bronze.crm_sales_details;
 SELECT COUNT(*) AS erp_cust_az12_rows FROM bronze.erp_cust_az12;
 SELECT COUNT(*) AS erp_loc_a101_rows FROM bronze.erp_loc_a101;
 SELECT COUNT(*) AS erp_px_cat_g1v2_rows FROM bronze.erp_px_cat_g1v2;
+
+EXEC bronze.load_bronze;
